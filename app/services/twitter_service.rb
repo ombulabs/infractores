4db612dd4cb47863c7426fa96348497ee012536a
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-08-29 12:46:48 -0300

    Improve the way to create a Tweet

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -13,26 +13,28 @@ class TwitterService
   # necessary {Tweet} records.
   def download_mentions
     opts = {}
-    opts[:since_id] = tweet.id if tweet = Tweet.last
+    if tweet = Tweet.last
+      opts[:since_id] = tweet.id
+    end
 
     tl = client.mentions_timeline opts
 
     tl.each do |tuit|
-      Tweet.create!(id: tuit.id, json: tuit.to_json)
+      Tweet.find_or_create!(tuit)
     end
   end
 
   private
 
-  def initialize
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
-    end
+    def initialize
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+      end
 
-    self.client = client
-  end
+      self.client = client
+    end
 
 end
