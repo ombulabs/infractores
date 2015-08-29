244c2967cd4ce62cef9c0a5a0e6576eef92fe6ec
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-08-29 12:47:26 -0300

    Added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'infractions#index'
+  get ':username' => 'users#show'
+  resource :users, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
