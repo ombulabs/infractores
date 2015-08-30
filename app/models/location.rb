9cb3bcc666f906c0d3df9babee3a635d19c42322
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-08-30 20:28:51 -0300

    Added Location and a way to build it

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,23 @@
 class Location < ActiveRecord::Base
-  belongs_to :infraction
+  has_many :infractions
+
+  # It uses the geo coordinates to find or create a location
+  #
+  # @param tweet [Tweet]
+  # @return [Location]
+  def self.find_or_create!(tweet)
+    return unless tweet.source.geo
+    
+    x = tweet.source.geo["coordinates"][0]
+    y = tweet.source.geo["coordinates"][1]
+    point = "POINT(#{x} #{y})"
+
+    unless result = find_by(lonlat: point)
+      result = new(lonlat: point)
+      result.save!
+    end
+
+    result
+  end
+
 end
