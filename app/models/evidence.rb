c99585812f0968466e330936bbcccde667d7a66c
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-08-30 20:28:33 -0300

    Added Evidence and a way to build it from a Tweet

--- app/models/evidence.rb
+++ app/models/evidence.rb
@@ -1,3 +1,19 @@
 class Evidence < ActiveRecord::Base
+  belongs_to :infraction
+
   mount_uploader :media, EvidenceUploader
+
+  # Builds an [Array] of [Evidence] records from a [Tweet] using the media
+  # entities
+  #
+  # @param tweet [Tweet]
+  # @return [Array]
+  def self.build_from(tweet)
+    result = []
+    tweet.source.extended_entities["media"].each do |media|
+      result << self.new(infraction: tweet.infraction,
+                         media: media["media_url_https"])
+    end
+    result
+  end
 end
