7a07d66a3a2a5c0b2cb64455ce3665c991d65467
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-05 20:36:24 -0300

    Change twitter_user_id type

--- db/migrate/20150829134520_create_users.rb
+++ db/migrate/20150829134520_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer :twitter_user_id
+      t.string :twitter_user_id
       t.string :username
       t.string :profile_image_url
       t.string :name
