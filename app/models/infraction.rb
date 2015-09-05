7a02dea3e6d50d6dea46f5a00f181a29becf59bc
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-05 20:34:44 -0300

    Added description to infraction and relationships

--- app/models/infraction.rb
+++ app/models/infraction.rb
@@ -3,15 +3,20 @@ class Infraction < ActiveRecord::Base
   belongs_to :location
 
   has_many :evidences
+  has_one :user, through: :tweet
 
   validates_presence_of :evidences
+  validates_presence_of :location
+
+  delegate :lat, :lng, to: :location
+  delegate :username, to: :user
 
   # Builds an {Infraction} using the {Tweet}
   #
   # @param tweet [Tweet]
   # @return [Infraction]
   def self.build_from(tweet)
-    result = self.new(tweet: tweet)
+    result = self.new(tweet: tweet, description: tweet.source.text)
     json = tweet.json
 
     begin
