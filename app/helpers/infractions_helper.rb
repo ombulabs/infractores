eaa330943664c78979a15bfa48409ed2726dd61d
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-06 18:23:39 -0300

    Get the right time

--- app/helpers/infractions_helper.rb
+++ app/helpers/infractions_helper.rb
@@ -1,5 +1,9 @@
 module InfractionsHelper
 
+  def decorated_time(infraction)
+    infraction.tweet.created_at
+  end
+
   def decorated_description(infraction)
     infraction.description.gsub(/(@infractoresba)/i,
                                 "<a href='https://twitter.com/InfractoresBA' target='_blank'>@InfractoresBA</a> ")
