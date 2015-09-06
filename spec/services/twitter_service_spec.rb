26349c8693a74eb1d754ca95da717efdbe71fde7
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-06 15:35:17 -0300

    Added VCR support

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -16,24 +16,41 @@ RSpec.describe "TwitterService" do
     end
   end
 
-  describe "#download_mentions" do
-    before do
-      allow(subject.client)
-        .to(receive(:mentions_timeline)
-              .and_return([valid_twitter_tweet]))
+  describe "#download_tweets" do
+    context "search and mentions return only one tweet" do
+      before do
+        allow(subject.client)
+          .to(receive(:mentions_timeline)
+                .and_return([valid_twitter_tweet]))
+        allow(subject.client)
+          .to(receive(:search)
+                .and_return([valid_twitter_tweet]))
+      end
+
+      it "creates one Tweet per mention and search" do
+        expect do
+          subject.download_tweets
+        end.to change(Tweet, :count).by(1)
+      end
+
+      it "creates one and only one Tweet per mention and search" do
+        expect do
+          subject.download_tweets
+          subject.download_tweets
+        end.to change(Tweet, :count).by(1)
+      end
     end
 
-    it "creates one Tweet per mention" do
-      expect do
-        subject.download_mentions
-      end.to change(Tweet, :count).by(1)
-    end
-
-    it "creates one and only one Tweet per mention" do
-      expect do
-        subject.download_mentions
-        subject.download_mentions
-      end.to change(Tweet, :count).by(1)
+    context "search returns 4 tweets" do
+      it "creates one Tweet per mention and search" do
+        VCR.use_cassette("search-tweets") do
+          VCR.use_cassette("mentions-tweets") do
+            expect do
+              subject.download_tweets
+            end.to change(Tweet, :count).by(15)
+          end
+        end
+      end
     end
   end
 end
