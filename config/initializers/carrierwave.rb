7d74b9a80f67c1f1e5936dfdb810e44721c49ab0
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-06 14:35:30 -0300

    Added region param

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,10 @@ if Rails.env.test?
 else
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+      provider: 'AWS',                        # required
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],                        # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
     }
     config.fog_directory  = "infractores-ba-#{Rails.env}"
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
