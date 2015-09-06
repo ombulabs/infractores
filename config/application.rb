c4d9fed3f3df1a9040eec7a5be4b1048cbb240eb
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-06 18:26:12 -0300

    Made the timezone and locale Spanish and BA

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,11 @@ module Infractores
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'America/Argentina/Buenos_Aires'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
     config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload if Rails.env.development?
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
