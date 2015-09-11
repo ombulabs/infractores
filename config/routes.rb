b157e1bc61e43dbb83ed5efa73d8aaf837a0026d
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-10 23:48:14 -0300

    Moved vanity url to the bottom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'infractions#index'
-  get ':username' => 'users#show'
   resources :users, only: [:show]
   resources :infractions, only: [:index, :show]
+  get 'leaderboard' => 'users#index'
+  get ':username' => 'users#show'
 end
