ddd34e9de76bca5aa845a9246ba88881b4f8caf3
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-10 23:49:01 -0300

    Spec in Spanish

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe Tweet do
     it "needs a user, json and id" do
       tweet = Tweet.new
       expect(tweet.valid?).to be_falsey
-      expect(tweet.errors.full_messages).to include("Id can't be blank")
-      expect(tweet.errors.full_messages).to include("Json can't be blank")
-      expect(tweet.errors.full_messages).to include("User can't be blank")
+      expect(tweet.errors.full_messages).to include("Id no puede estar en blanco")
+      expect(tweet.errors.full_messages).to include("Json no puede estar en blanco")
+      expect(tweet.errors.full_messages).to include("User no puede estar en blanco")
     end
 
     it "needs a unique id" do
       FactoryGirl.create(:tweet, id: "123")
       tweet = FactoryGirl.build(:tweet, id: "123")
       expect(tweet.valid?).to be_falsey
-      expect(tweet.errors.full_messages).to include("Id has already been taken")
+      expect(tweet.errors.full_messages).to include("Id ya está en uso")
     end
   end
 end
