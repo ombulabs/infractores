86fcbbb9ad7df6f762c00ba01b411ba013742c93
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-10 23:56:11 -0300

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911020838) do
+ActiveRecord::Schema.define(version: 20150911025204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,5 +53,7 @@ ActiveRecord::Schema.define(version: 20150911020838) do
     t.datetime "updated_at",                    null: false
     t.integer  "infractions_count", default: 0
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
 end
