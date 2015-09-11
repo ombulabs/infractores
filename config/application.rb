65d9588daa75edc2b18e0e288bc40a2ce3af15b0
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-11 00:47:40 -0300

    Hack for Postgis

--- config/application.rb
+++ config/application.rb
@@ -25,3 +25,26 @@ module Infractores
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+# in config/application.rb, before the application loads
+class ActiveRecordOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
+
+    ActiveSupport.on_load(:active_record) do
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
