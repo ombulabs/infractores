6dd31fbf210ae60933b85790b822e45cb0872859
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-10 23:47:06 -0300

    Added helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def calculate_class(*arr)
+    arr.any? {|x| current_page?("/#{x}") } ? 'active' : ''
+  end
+
+  def decorated_username(infraction)
+    username = infraction.username
+    "<a href='https://twitter.com/#{username}' target='_blank'>@#{username}</a>"
+  end
 end
