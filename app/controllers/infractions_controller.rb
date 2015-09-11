989eba4bf6e1a97ae95f944d817517e4ab1e6a5c
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-10 23:55:54 -0300

    Added filtering by user

--- app/controllers/infractions_controller.rb
+++ app/controllers/infractions_controller.rb
@@ -1,12 +1,22 @@
 class InfractionsController < ApplicationController
+  before_filter :load_scope, only: [:index]
 
   # GET /infractions
   def index
-    @infractions = Infraction.includes(:evidences, :tweet, :location, :user).order("ID DESC")
+    @infractions = @scope.order("infractions.id DESC")
   end
 
   # GET /infractions/:id
   def show
     @infraction = Infraction.includes(:user).find_by(tweet_id: params[:id])
+  end
+
+  private
+
+  def load_scope
+    @scope = Infraction.includes(:evidences, :tweet, :location, :user)
+    if params[:username]
+      @scope = @scope.where("users.username" => params[:username])
+    end
   end
 end
