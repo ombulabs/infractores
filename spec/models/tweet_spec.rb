bf1a73c2f5e1c488a2e9947ada343637ebb35de4
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-09-18 08:34:24 -0300

    Added coverage.

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Tweet do
+  let!(:valid_tweet) { FactoryGirl.create(:valid_tweet) }
+
+  describe "#created_at" do
+    it "returns the tweet creation date" do
+      time = Time.parse("2015-08-28 12:40:47 -0300")
+      expect(valid_tweet.created_at).to eql time
+    end
+  end
+
+  describe "#source_url" do
+    it "returns a twitter status url" do
+      username = valid_tweet.user.username
+      expect(valid_tweet.source_url).to eql "https://twitter.com/#{username}/status/637288728814317568"
+    end
+  end
+
   describe "#valid?" do
     it "needs a user, json and id" do
       tweet = Tweet.new
