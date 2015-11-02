d0387aaa7ca78ad10a1755aedb18181bb27b21c6
Sirko Sittig <sirko.sittig@gmail.com>
2015-11-02 19:29:45 -0300

    move lon and lat to infraction model

--- spec/models/infraction_spec.rb
+++ spec/models/infraction_spec.rb
@@ -7,8 +7,19 @@ RSpec.describe Infraction do
     context "valid infraction tweet" do
       subject { Infraction.build_from(tweet) }
 
-      it "builds a Location and associates it with the Infraction" do
-        expect(subject.location).to be
+      it "extracts geoinfos from tweet" do
+        expect(subject.lat).to eq(-34.58255407)
+        expect(subject.lon).to eq(-58.43625901)
+      end
+
+      it "invalid without lat" do
+        subject.lat = nil
+        expect(subject).to_not be_valid
+      end
+
+      it "invalid without lon" do
+        subject.lon = nil
+        expect(subject).to_not be_valid
       end
 
       it "builds many Evidences and associates it with the Infraction" do
