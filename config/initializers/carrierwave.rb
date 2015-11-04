2ff35d1937366553eb2eae163542447d63fa7e58
Sirko Sittig <sirko.sittig@gmail.com>
2015-11-04 13:19:59 -0300

    always use sa-east-1 for AWS region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ else
       provider: 'AWS',                        # required
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],                        # required
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION']
+      region: 'sa-east-1'
     }
     config.fog_directory  = "infractores-ba-#{Rails.env}"
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
